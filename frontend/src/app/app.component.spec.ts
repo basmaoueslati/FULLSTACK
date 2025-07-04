dbacaced8b509a00fc58e382d8f26adb022efb6b
basmaoueslati <47010064+basmaoueslati@users.noreply.github.com>
2025-07-04 15:40:57 +0100

    Update app.component.spec.ts

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -20,13 +20,13 @@ describe('CustomerService', () => {
 });
 
   it(`should have as title 'frontend'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(CustomerService);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('frontend');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(CustomerService);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain('frontend app is running!');
