434a03b4d490640ddd668780d22dead223c55b89
basmaoueslati <47010064+basmaoueslati@users.noreply.github.com>
2025-07-04 15:34:44 +0100

    Update app.component.spec.ts

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,22 +1,23 @@
 import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { CustomerService } from './customer.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
-describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-      imports: [HttpClientTestingModule] 
-    }).compileComponents();
+describe('CustomerService', () => {
+  let service: CustomerService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule], 
+      providers: [CustomerService]
+    });
+
+    service = TestBed.inject(CustomerService);
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  it('should be created', () => {
+    expect(service).toBeTruthy();
   });
+});
 
   it(`should have as title 'frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
