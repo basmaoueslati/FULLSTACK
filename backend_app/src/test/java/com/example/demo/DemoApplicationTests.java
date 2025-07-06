783b5b296b335b48208178ed86452bb77068c328
basmaoueslati <47010064+basmaoueslati@users.noreply.github.com>
2025-07-06 12:30:08 +0100

    Update DemoApplicationTests.java

--- backend_app/src/test/java/com/example/demo/DemoApplicationTests.java
+++ backend_app/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,11 +3,12 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
+})
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
