37f875aaee9828366bd569874b1c1bf188253c93
basmaoueslati <47010064+basmaoueslati@users.noreply.github.com>
2025-07-06 12:46:15 +0100

    Update DemoApplicationTests.java

--- backend_app/src/test/java/com/example/demo/DemoApplicationTests.java
+++ backend_app/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,21 +2,17 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@SpringBootTest(classes = {}, webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootTest(classes = DemoApplicationTests.EmptyConfig.class)
 class DemoApplicationTests {
 
     @Test
     void contextLoads() {
     }
-}
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan(excludeFilters = @ComponentScan.Filter(type = org.springframework.context.annotation.FilterType.REGEX, pattern = "com\\.example\\.demo\\..*"))
-class NoDbTestConfig {
-    // no beans will be loaded from your app
+    @Configuration
+    static class EmptyConfig {
+        // no beans
+    }
 }
