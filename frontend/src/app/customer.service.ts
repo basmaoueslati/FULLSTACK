d120471237219a7e56cd4a40643365fb0f3895b6
basmaoueslati <47010064+basmaoueslati@users.noreply.github.com>
2025-07-12 11:05:40 +0100

    Update customer.service.ts

--- frontend/src/app/customer.service.ts
+++ frontend/src/app/customer.service.ts
@@ -14,7 +14,7 @@ export interface Customer {
 
 export class CustomerService {
 
-  private apiUrl = '/customers';
+  private apiUrl = 'http://backend.devops.com/customers';
 
   constructor(private http: HttpClient) {}
 
